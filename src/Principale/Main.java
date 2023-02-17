f38776d61bad48c82467e8190f3cbee5f983e255
GabboV <g.visovan@studenti.unibs.it>
2023-02-17 08:15:38 +0100

    G

--- src/Principale/Main.java
+++ src/Principale/Main.java
@@ -3,8 +3,9 @@ package Principale;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.print("Progetto Ristorante");
+		System.out.print("Progetto Ristorante");
+		System.out.print("Progetto Ristorante");
 	}
 
 }
