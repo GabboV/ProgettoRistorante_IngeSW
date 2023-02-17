305cb66be1f793e302182803bf700751194067af
GabboV <g.visovan@studenti.unibs.it>
2023-02-17 10:19:05 +0100

    Aggiunta linea

--- src/Principale/Main.java
+++ src/Principale/Main.java
@@ -6,6 +6,8 @@ public class Main {
 		System.out.print("Progetto Ristorante");
 		System.out.print("Progetto Ristorante");
 		System.out.print("Progetto Ristorante");
+		System.out.print("Progetto Ristorante");
+
 	}
 
 }
