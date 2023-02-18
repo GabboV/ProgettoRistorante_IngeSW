8322cc180bdac0ec92ccc6123f3105f711f22fe9
Kevin <k.liu@studenti.unibs.it>
2023-02-18 10:23:27 +0100

    Update

--- src/Principale/AddettoPrenotazioni.java
+++ src/Principale/AddettoPrenotazioni.java
@@ -1,5 +1,35 @@
 package Principale;
 
+import java.util.ArrayList;
+
 public class AddettoPrenotazioni {
+	
+	private ArrayList<Prenotazione> prenotazioni;
+
+	public AddettoPrenotazioni() {
+		super();
+		this.prenotazioni = new ArrayList<Prenotazione>();
+	}
+	
+	public void addPrenotazione(Prenotazione p) {
+		this.prenotazioni.add(p);
+	}
+	
+	/**
+	 * 
+	 */
+	public void removePrenotazioniScadute() {
+		
+	}
+	
+	/**
+	 * 
+	 * @return Prenotazione
+	 */
+	public void creaPrenotazione() {
+		
+	}
+	
+	
 
 }
